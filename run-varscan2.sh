a59b0e4b8c7f6e6bc26f7520e8fbdfcb2a7307ca
Ali Mahdipour-Shirayeh <ali.mahdipour@gmail.com>
2019-11-13 18:09:41 -0500

    Update run-varscan2.sh

--- run-varscan2.sh
+++ run-varscan2.sh
@@ -1,10 +1,16 @@
+
 #!/bin/bash
 #
 #$ -cwd
-#$ -S /bin/bash
 
 . /etc/bashrc
 
-module load samtools/1.2
+module load varscan/2.3.6
+module load gatk/3.0-0
 
-java -jar /cluster/tools/software/varscan/2.3.6/VarScan.jar copynumber run_mpileup_tumor_vs_normal.sh.o213767 /cluster/projects/tiedemannlab/WES/data/mpileup/RTBM_199T__P6_WES_S13_sorted.varscan –mpileup 1 –data-ratio 0.21
+java -jar /cluster/tools/software/varscan/2.3.6/VarScan.jar copynumber \ 
+/cluster/projects/WES/data/mpileup/sample_test_vs_normal_WES.mpileup.bcf \ 
+/cluster/projects/WES/data/varscan2/sample_test_vs_normal_WES.mpileup.copynumber \ 
+ –mpileup 1 –data-ratio 0.21
+ 
+ 
