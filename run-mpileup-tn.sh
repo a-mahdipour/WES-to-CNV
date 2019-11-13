2afa3b423c82036d3add87af4a79f35d3843e92c
Ali Mahdipour-Shirayeh <ali.mahdipour@gmail.com>
2019-11-13 18:00:04 -0500

    Update run-mpileup-tn.sh

--- run-mpileup-tn.sh
+++ run-mpileup-tn.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #
 #$ -cwd
@@ -7,4 +8,7 @@
 
 module load samtools/1.2
 
-samtools mpileup -q 1 -f /cluster/projects/tiedemannlab/Reference_Files/ref/refdata-GRCh38-2.1.0/genome.fa /cluster/projects/tiedemannlab/WES/data/RTBM_199_Buccal_WES_S14_sorted.bam /cluster/projects/tiedemannlab/WES/data/RTBM_199T__P6_WES_S13_sorted.bamx
+samtools mpileup -q 1 -f /cluster/projects/ref/refdata-GRCh38-2.1.0/genome.fa \
+/cluster/projects/WES/data/Sample_normal_WES_sorted.bam \
+/cluster/projects/tiedemannlab/WES/data/Sample_test_WES_sorted.bam
+
