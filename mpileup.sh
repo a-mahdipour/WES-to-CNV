660dabecf57f96e5e8aa63bdeded03c8a5935fa0
Ali Mahdipour-Shirayeh <ali.mahdipour@gmail.com>
2019-11-13 17:23:09 -0500

    Update mpileup.sh

--- mpileup.sh
+++ mpileup.sh
@@ -8,6 +8,9 @@
 
 module load samtools/1.2
 
-samtools mpileup −f /cluster/projects/tiedemannlab/Reference_Files/ref/refdata-GRCh38-2.1.0/genome.fa −Q 20 /cluster/projects/tiedemannlab/WES/data/RTBM_199_Buccal_WES_S14_sorted.bam | gzip > /cluster/projects/tiedemannlab/WES/data/RTBM_199_Buccal_WES_S14_sorted.pileup.gz
+samtools mpileup −f /cluster/projects/ref/refdata-GRCh38-2.1.0/genome.fa \
+−Q 20 \
+/cluster/projects/tiedemannlab/WES/data/RTBM_199_Buccal_WES_S14_sorted.bam \
+| gzip > /cluster/projects/tiedemannlab/WES/data/RTBM_199_Buccal_WES_S14_sorted.pileup.gz
 
 
