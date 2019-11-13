1eac3615bcc8f77dce067097e76863b04f8fd208
Ali Mahdipour-Shirayeh <ali.mahdipour@gmail.com>
2019-11-13 18:31:33 -0500

    Update run-sequenza.sh

--- run-sequenza.sh
+++ run-sequenza.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #
 #$ -cwd
@@ -6,11 +7,10 @@
 
 module load python/2.7.3
 
-pypy /data/home/mpx155/R/packages/3.1.0/sequenza/exec/sequenza-utils.py bam2seqz \
- --fasta /cluster/projects/tiedemannlab/Reference_Files/ref/refdata-GRCh38-2.1.0/genome.fa \
- -n /cluster/projects/tiedemannlab/WES/data/RTBM_199_Buccal_WES_S14_sorted.bam \
- -t /cluster/projects/tiedemannlab/WES/data/RTBM_199T__P6_WES_S13_sorted.bam \
- -gc /cluster/projects/tiedemannlab/Reference_Files/ref/refdata-GRCh38-2.1.0/genome.fa \
- --chromosome chr1 | \
-pypy /data/home/mpx155/R/packages/3.1.0/sequenza/exec/sequenza-utils.py binning -w 50 | \
-gzip > RTBM_199_Buccal_WES_S14_sorted_vs_Buccal_chr1_bin50.seqz.gz
+pypy /data/home/R/packages/3.1.0/sequenza/exec/sequenza-utils.py bam2seqz \
+ --fasta /cluster/projects/ref/refdata-GRCh38-2.1.0/genome.fa \
+ -n /cluster/projects/WES/data/RTBM_199_Buccal_WES_S14_sorted.bam \
+ -t /cluster/projects/WES/data/RTBM_199T__P6_WES_S13_sorted.bam \
+ -gc /cluster/projects/ref/refdata-GRCh38-2.1.0/genome.fa | \
+gzip > Sample_test_vs_normal_chr1_bin50.seqz.gz
+
