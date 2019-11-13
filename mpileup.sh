35049a8b9c41a8f1207bf4aa2c2c4509f48575be
Ali Mahdipour-Shirayeh <ali.mahdipour@gmail.com>
2019-11-13 13:11:31 -0500

    Add files via upload

---
+++ mpileup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#$ -cwd
+#$ -S /bin/bash
+#
+
+. /etc/bashrc
+
+module load samtools/1.2
+
+samtools mpileup −f /cluster/projects/tiedemannlab/Reference_Files/ref/refdata-GRCh38-2.1.0/genome.fa −Q 20 /cluster/projects/tiedemannlab/WES/data/RTBM_199_Buccal_WES_S14_sorted.bam | gzip > /cluster/projects/tiedemannlab/WES/data/RTBM_199_Buccal_WES_S14_sorted.pileup.gz
+
+
